968f193df31b70c1605b15db22642ec7876826ab
SmoulTheSloul <135207773+SmoulTheSloul@users.noreply.github.com>
2023-06-01 17:41:51 +0800

    Update takeinfo.py

--- takeinfo.py
+++ takeinfo.py
@@ -3,12 +3,12 @@ from pymavlink import mavutil
 import time
 import argparse  
 parser = argparse.ArgumentParser()
-parser.add_argument('--connect', default='127.0.0.1:14550')
+parser.add_argument('--connect', default='/dev/ttyUSB0')
 args = parser.parse_args()
 
 # Connect to the Vehicle
 print 'Connecting to vehicle on: %s' % args.connect
-vehicle = connect(args.connect, baud=57600, wait_ready=True)
+vehicle = connect(args.connect, baud=921600, wait_ready=True)
 
 
 print("Connection to Cube Orange Succesful!")
