c844bb87674bfc088d17e71200511ce4aef166e3
SmoulTheSloul <135207773+SmoulTheSloul@users.noreply.github.com>
2023-06-01 15:16:52 +0800

    Create takeinfo.py

---
+++ takeinfo.py
@@ -0,0 +1,20 @@
+from dronekit import connect, VehicleMode, LocationGlobalRelative
+from pymavlink import mavutil
+import time
+import argparse  
+parser = argparse.ArgumentParser()
+parser.add_argument('--connect', default='127.0.0.1:14550')
+args = parser.parse_args()
+
+# Connect to the Vehicle
+print 'Connecting to vehicle on: %s' % args.connect
+vehicle = connect(args.connect, baud=921600, wait_ready=True)
+
+
+print("Connection to Cube Orange Succesful!")
+print("Mode: %s" % vehicle.mode.name)
+time.sleep(2)
+# Close vehicle object before exiting script
+vehicle.close()
+
+print("Completed")
