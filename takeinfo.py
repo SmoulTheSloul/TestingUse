50ebd4c0b584c5661a07b514e8e913a4f35168ee
SmoulTheSloul <135207773+SmoulTheSloul@users.noreply.github.com>
2023-06-01 15:29:36 +0800

    Update takeinfo.py

--- takeinfo.py
+++ takeinfo.py
@@ -8,7 +8,7 @@ args = parser.parse_args()
 
 # Connect to the Vehicle
 print 'Connecting to vehicle on: %s' % args.connect
-vehicle = connect(args.connect, baud=921600, wait_ready=True)
+vehicle = connect(args.connect, baud=57600, wait_ready=True)
 
 
 print("Connection to Cube Orange Succesful!")
